325dce99757bbc851add4e31a7b4943caa0b9e15
Isambard Wheeler-artwell <isambardwheeler-artwell@Isambards-Air.home>
2018-07-05 09:54:22 +0100

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import CommentBox from './CommentBox';
+import CommentList from './CommentList';
+
+export default () => {
+
+    return (
+        
+        <div>
+            Im the app
+            < CommentBox />
+            < CommentList />
+        </div>
+
+    );
+
+};
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+// Searches the index.html in public folder
+ReactDOM.render(
+    <App/>,
+    document.querySelector('#root'))
