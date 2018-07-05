081df4d5841b8fdb4b0e88ad88b9d7a37413955a
Isambard Wheeler-artwell <isambardwheeler-artwell@Isambards-Air.home>
2018-07-05 14:38:26 +0100

    before and after method for test

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import CommentBox from 'component/CommentBox';
+
+// Components
+import CommentBox from 'components/CommentBox';
+
+let wrapped;
+
+beforeEach(() => {
+    wrapped = mount(<CommentBox />);
+});
+
+// Remove the component from the DOM
+afterEach(() => {
+    wrapped.unmount();
+});
 
 it ('has a text area and a button', () => {
 
     // Mount is for full Document Full Rendering
-
     const wrapped = mount(<CommentBox/>);
 
     console.log(wrapped.find('textarea'));
     console.log(wrapped.find('button'));
 
     expect(wrapped.find('textarea').length).toEqual(1);
-    expect(wrapped.find('button').length).toEqual(1 );
-
-
+    expect(wrapped.find('button').length).toEqual(1);
 
 });
