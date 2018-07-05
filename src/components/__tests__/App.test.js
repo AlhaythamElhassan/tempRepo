cb56ce53e5007bba7a9fd1243226450cf0f514dc
Isambard Wheeler-artwell <isambardwheeler-artwell@Isambards-Air.home>
2018-07-05 12:07:52 +0100

    One test working

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import App from '../App'
-
-import { shallow } from 'enzyme';
+import App from '../App';
 import CommentBox from '../CommentBox';
+import { shallow, mount, render, configure } from 'enzyme';
+import * as Adapter from 'enzyme-adapter-react-16';
+
 
 // shallow is a function that renders just the component and none of it's children
 
 it ('shows a comment box', () => {
 
+    var enzyme = require('enzyme');
+
     // wrapped is the same as component
     const wrapped = shallow(<App />);
 
-    console.log(wrapped.find(CommentBox));
-
     // find is an array
-
     expect(wrapped.find(CommentBox).length).toEqual(1);
-
-
 
 
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,9 +1,10 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+// import * as ReactSixteenAdapter from 'enzyme-adapter-react-16';
+
 
 // The reason we call this file setupTests
 // is because jest will look for this file and run it before executting other code
-
 // We use Enzyme to configure our tests
 
-Enzyme.configure({ adpater: new Adapter() });
+Enzyme.configure({ adapter: new Adapter() });
