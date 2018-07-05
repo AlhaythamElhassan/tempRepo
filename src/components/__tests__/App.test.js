ab50d0e5e4f5384b141b078debbd3c384a928756
Isambard Wheeler-artwell <isambardwheeler-artwell@Isambards-Air.home>
2018-07-05 12:22:49 +0100

    Added two unit tests

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import App from '../App';
-import CommentBox from '../CommentBox';
 import { shallow, mount, render, configure } from 'enzyme';
-import * as Adapter from 'enzyme-adapter-react-16';
 
+//Components
+import CommentBox from '../CommentBox';
+import CommentList from '../CommentList';
 
 // shallow is a function that renders just the component and none of it's children
-
 it ('shows a comment box', () => {
-
-    var enzyme = require('enzyme');
 
     // wrapped is the same as component
     const wrapped = shallow(<App />);
+
+    console.log(wrapped.find(CommentBox));
 
     // find is an array
     expect(wrapped.find(CommentBox).length).toEqual(1);
 
 
 });
+
+
+it("shows at least one comment list on page ", () => {
+
+    const component = shallow(<App/>);
+
+    console.log(component.find(CommentList));
+
+    expect(component.find(CommentList).length).toEqual(1);
+
+});
+
