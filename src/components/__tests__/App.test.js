950c38bf8d230ef544a61cb9031650fdd169eaa1
Isambard Wheeler-artwell <isambardwheeler-artwell@Isambards-Air.home>
2018-07-05 10:08:37 +0100

    working on tests with shallow enzyme

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import App from '../App'
 
-import {}
+import { shallow } from 'enzyme';
+import CommentBox from '../CommentBox';
+
+// shallow is a function that renders just the component and none of it's children
 
 it ('shows a comment box', () => {
+
+    // wrapped is the same as component
+    const wrapped = shallow(<App />);
+
+    console.log(wrapped.find(CommentBox));
+
+    // find is an array
+
+    expect(wrapped.find(CommentBox).length).toEqual(1);
+
+
 
 
 });
